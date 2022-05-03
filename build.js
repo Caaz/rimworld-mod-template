63d83d528d47abdb4cdf4701e7a57886bdc1c5e2
Daniel Caaz <dcaazy@gmail.com>
2022-05-02 19:29:42 -0700

    Add environmental options to metadata.json

--- build.js
+++ build.js
@@ -1,22 +1,35 @@
-const fs = require('fs')
-const pug = require('pug')
-var data = require('./metadata.json')
-function basicError(err) {
-    if (err) console.error(err)
+import fs from 'node:fs';
+import process from 'node:process';
+import pug from 'pug';
+
+import metadata from './metadata.json' assert { type:"json" };
+
+const env = process.argv[2] || 'dev';
+console.log('Building for environment %s', env);
+
+const data = {
+    ...metadata["default"],
+    ...(metadata[env] || {})
 }
 
-fs.readdir('Pug', (err, files) => {
-    files.forEach(file => {
-        if(file.endsWith(".pug")) {
-            let name = file.substring(0, file.indexOf("."))
-            let xmlName = name + "/" + name + ".xml"
-            if(name == "csproj")
-                xmlName = data.assemblyName + ".csproj"
-            else
-                fs.mkdir(name, (err) => { })
-            console.log("Rendering " + name)
-            renderer = pug.compileFile("Pug/" + file, {pretty: true})
-            fs.writeFile(xmlName, renderer(data), basicError)
-        }
-    });
+fs.readdir('Pug', (error, files) => {
+	for (const file of files) {
+		if (file.endsWith('.pug')) {
+			const name = file.slice(0, Math.max(0, file.indexOf('.')));
+			let xmlName = name + '/' + name + '.xml';
+			if (name === 'csproj') {
+				xmlName = data.assemblyName + '.csproj';
+			} else {
+				fs.mkdir(name, _ => {});
+			}
+
+			console.log('Rendering ' + name);
+			const renderer = pug.compileFile('Pug/' + file, {pretty: true});
+			fs.writeFile(xmlName, renderer(data), error_ => {
+				if (error_) {
+					console.error(error_);
+				}
+			});
+		}
+	}
 });
